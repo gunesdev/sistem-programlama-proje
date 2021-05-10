ea86f7fa8453b576e9ab1e47f4885448610156a4
sevvallyildizzz <ssevvalyyildiz@hotmail.com>
2021-05-10 17:19:14 +0300

    Komut satırı argümanları için arayüz yazıldı.

--- main.c
+++ main.c
@@ -4,7 +4,16 @@
 #include "dllist.h"
 #include "jval.h"
 
-int main(int argc, char **argv){
-	printf("Hello World!");
-	return 0;
+int main(int argc, char **argv)
+{
+    if( argc > 4 ) {
+       printf("├──Hatali Kullanim. Fazla Parametre Girildi──├\n├──────────────Doğru Kullanim────────────├ : \n├──>$kripto -d giriş_metin cikis_metin───├\n├                   ya  da               ├\n├──>$kripto -e giriş_metin cikis_metin───├\n");
+    }
+    else if( argc < 4 ) {
+        printf("\n├──Hatali Kullanim. Eksik Parametre Girildi──├\n├──────────────Doğru Kullanim────────────├ : \n├──>$kripto -d giriş_metin cikis_metin───├\n├                   ya  da               ├\n├──>$kripto -e giriş_metin cikis_metin───├\n");
+    }
+    else if( argc == 4 ) {
+        printf("├──────────Girdiğiniz parametreler :  \n├           ├──%s\n├           ├──%s\n├           ├──%s\n", argv[1],argv[2],argv[3]);
+     }
+    return 0;
 }

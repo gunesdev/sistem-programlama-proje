67e7c0e01d43bc32f31e6305503aa57df9205cda
sevvallyildizzz <ssevvalyyildiz@hotmail.com>
2021-05-10 20:58:26 +0300

    -e ve -d argümanları için koşullar eklendi.

--- main.c
+++ main.c
@@ -14,6 +14,20 @@ int main(int argc, char **argv)
     }
     else if( argc == 4 ) {
         printf("├──────────Girdiğiniz parametreler :  \n├           ├──%s\n├           ├──%s\n├           ├──%s\n", argv[1],argv[2],argv[3]);
+	     /*---------------------Begin Read .Kilit -----------------------*/
+	     
+	     /*---------------------End Read .Kilit -----------------------*/   
+             /*---------------------Begin Encryption -----------------------*/
+         if(strcmp(argv[1], "-e") == 0){
+            
+         }
+             /*---------------------End Encryption -----------------------*/
+            /*---------------------Begin Decription -----------------------*/
+         else if(strcmp(argv[1], "-d") == 0){
+            
+         }
+            /*---------------------End Decription -----------------------*/
+         else{printf("├───────────────────HATA -e ya da -d girilmeli───────────────────├\n");}
      }
     return 0;
 }
